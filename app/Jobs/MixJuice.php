51e7b6c6cd586c0377df07bc07fb6bfb3387dfa7
twij <twij@pm.me>
2019-09-03 00:59:51 +0100

    Temp fix big numbers

--- app/Jobs/MixJuice.php
+++ app/Jobs/MixJuice.php
@@ -79,6 +79,8 @@ class MixJuice implements ShouldQueue
         $this->vg_amount = $this->getAmount($this->vg);
         $this->pg_amount = $this->getAmount($this->pg);
 
+        dd($this);
+
         if ($this->base_type == "VG") {
             $this->deductVG($this->nicotine_ml);
         } else {
@@ -108,11 +110,11 @@ class MixJuice implements ShouldQueue
      *
      * @param Float $percent
      *
-     * @return Float Amount in ml
+     * @return  Amount in ml
      */
-    public function getAmount(Float $percent): Float
+    public function getAmount(Float $percent)
     {
-        return (Float)number_format(($percent * $this->quantity) / 100, 2);
+        return number_format(($percent * $this->quantity) / 100, 2);
     }
 
     /**
@@ -142,6 +144,7 @@ class MixJuice implements ShouldQueue
     {
         $this->vg_amount -= $amount;
         if ($this->vg_amount < 0) {
+            dd($this);
             $this->pg_amount + $this->vg_amount;
             $this->vg_amount = 0;
         }
