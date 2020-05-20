d7da9860704e84f11d9a55244a68410d2803939d
twij <30731724+twij@users.noreply.github.com>
2020-05-20 18:36:13 +0100

    urmom

---
+++ public/urmom/urmom.php
@@ -0,0 +1,127 @@
+<h1>Making Urmom Coil</h1>
+She's ugly but she puts out.<br><br>
+
+IRC user Banks has created an <a href="http://imgur.com/a/W0ReE" target="_blank">alternative guide</a> which has more detail. His method is slightly different to mine. He wraps the top post the other way and 
+doesn't pull a core out of the rope. I highly recommend pulling a core out as it improves wicking substantially. He is also using the wrong size mesh. 
+I believe this is the free piece of #100 mesh that The Mesh Company send out with orders. Again I would recommend using #400 mesh as it wicks faster. Besides those things his guide seems to be good.<br><br>
+
+<img src="urmom/picture015.jpg" alt="pic" width="1000"><br>
+Drain juice and remove old wick.<br><br>
+
+<img src="urmom/picture017.jpg" alt="pic" width="1000"><br>
+Take SS rope. I use about 3cm. You can remove 1 core from the cable if you are 
+planning on using thick juice. To do this just grab some pliers and pull one 
+strand. The rest of the core should come out with it. You should flame the SS 
+rope to remove any packing chemicals or hand grease.<br><br>
+
+<img src="urmom/picture020.jpg" alt="pic" width="1000"><br>
+Take a piece of SS mesh. I use around 2.5cm by 5cm of 400 mesh. Flame the mesh to 
+remove any unwanted chemicals that may be present.<br><br>
+
+<img src="urmom/picture021.jpg" alt="pic" width="1000"><br>
+Roll the mesh around the rope. I try and stretch it long ways which helps to open 
+it out a bit for thick juice. For 100% VG juice you may want to use 500 mesh 
+instead. Crimp the top of the mesh into the rope with some plier to stop 
+it unravelling.<br><br>
+-
+<img src="urmom/picture024.jpg" alt="pic" width="1000"><br>
+Drip some e-liquid into the mesh and light it on fire. Allow it to burn all of the juice 
+off. You may want to complete this a few times. It will help to build up a layer of oxidisation.<br><br>
+
+<img src="urmom/picture027.jpg" alt="pic" width="1000"><br>
+Insert the wick into the wick hole and align it so the wick is higher than the centre post, but low 
+enough to get the top cap on. It doesn't need to touch the bottom it 
+should hold itself in place if it is thick enough.<br><br>
+
+<img src="urmom/picture031.jpg" alt="pic" width="1000"><br>
+Cut some wire and trap it under the negative screw. Wrap it around the wick starting from around 
+the back and wrap it anti-clockwise. This will reduce the length of the wire that is not on the wick.<br><br>
+
+<img src="urmom/picture033.jpg" alt="pic" width="1000"><br>
+Wrap the wire until it is inline with the washer at the top of the centre post. Keep the wraps nice and 
+close together. Go all the way round the centre post in a figure of 8. It's more of a figure of 9 I guess. This will also 
+help to reduce the wire that is not next to the wick.<br><br>
+
+<img src="urmom/picture035.jpg" alt="pic" width="1000"><br>
+Thread the wire behind the wick and make a coil over the top of the coil going upwards. Cover all the gaps 
+in the first coil. This will make the wire short and reduce the resistance so it heats up quickly. It will 
+also create a nice thick coil which will create better flavour. The more air that doesn't go over 
+the coil, the worse the flavour will be.<br><br>
+
+<img src="urmom/picture036.jpg" alt="pic" width="1000"><br>
+When you're at the bottom, grab both wires together and wrap them all the way around the negative screw. 
+This will help to keep them trapped together. Tighten the screw down.<br><br>
+
+<img src="urmom/picture037.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture038.jpg" alt="pic" width="1000"><br>
+Should look something like this.<br><br>
+
+<img src="urmom/picture039.jpg" alt="pic" width="1000"><br>
+Push both the wires leading from the wick to the centre post together to short them into 
+one and to get as close to the wick as possible. This will help them not to glow.<br><br>
+
+<img src="urmom/picture041.jpg" alt="pic" width="1000"><br>
+Crush the wires going from the negative screw to the wick down onto the base as much as 
+possible to short them to the deck. This will help these wires not to glow brighter than the coil.<br><br>
+
+<img src="urmom/picture043.jpg" alt="pic" width="1000"><br>
+OK maybe I lied.. She is pretty cute.<br><br>
+
+<img src="urmom/picture049.jpg" alt="pic" width="1000"><br>
+Begin pulsing the coil on a mechanical mod. It will not light up evenly at first but it should start 
+to quickly. If you see any hot spots then stop pulsing and move the wires up or down a bit with a 
+screwdriver or knife. They will return to pretty much where they were but may have lost the hot spot.<br><br>
+
+<img src="urmom/picture051.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture052.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture053.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture054.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture055.jpg" alt="pic" width="1000"><br><br>
+
+<img src="urmom/picture056.jpg" alt="pic" width="1000"><br>
+Add a little joice to the coil to cool it down. It will create lots of vapour.<br><br>
+
+<img src="urmom/picture057.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture058.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture059.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture060.jpg" alt="pic" width="1000"><br><br>
+
+<img src="urmom/picture060.jpg" alt="pic" width="1000"><br>
+Fill the tank with juice. <br><br>
+
+<img src="urmom/picture062.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture063.jpg" alt="pic" width="1000"><br>
+Test the device and make sure that the wick can hold enough juice to not dry up 
+before you have finished taking a hit. If there are any hot spots still it should 
+be fairly obvious. If there is, drain the juice and keep pulsing.<br><br>
+
+<img src="urmom/picture064.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture065.jpg" alt="pic" width="1000"><br><br>
+<img src="urmom/picture066.jpg" alt="pic" width="1000"><br><br>
+
+<img src="urmom/picture072.jpg" alt="pic" width="1000"><br>
+Check the resistance on a multimeter or other reliable low ohm checker like an ESR meter. 
+Throw your Chinese ohm reader in the sea.<br><br>
+
+That's it. Urmom is easy.<br><br>
+
+<b>What I use (UK shop):</b><br>
+Mesh: #400 - TWILL WEAVE - 25 Micron or 50 AWG <a href="http://vape-atomizer-mesh.com/products/-400-x-0.025mm--25-Micron-or-50-AWG--Super-Fine-Vape-Mesh---TWILL-WEAVE--316-Marine-Grade-Stainless-Steel---150-x-250mm-Sheet-2459.html" target="_blank">Small</a> / 
+<a href="http://vape-atomizer-mesh.com/products/-400-x-0.025mm--25-Micron-or-50-AWG--Super-Fine-Vape-Mesh---TWILL-WEAVE--316-Marine-Grade-Stainless-Steel---210-x-300mm-Sheet--A4--2460.html" target="_blank">A4</a> / 
+<a href="http://vape-atomizer-mesh.com/products/-400-x-0.025mm--25-Micron-or-50-AWG--Super-Fine-Vape-Mesh---TWILL-WEAVE--316-Marine-Grade-Stainless-Steel---300-x-420mm-Sheet--A3--2461.html" target="_blank">A3</a><br>
+Rope: 2mm Diameter (2,000 Micron or 12 AWG) - S316 Marine Grade - 7 x 7 Wire Rope (other sizes will work fine) 
+<a href="http://vape-atomizer-mesh.com/products/2mm-Diameter--2-000-Micron-or-12-AWG----S316-Marine-Grade---7-x-7-Wrie-Cable-Rope---1-Metre--2419.html" target="_blank">1 meter</a><br>
+Ribbon Wire: 1mm x 0.1mm Thickness Kanthal A1 Ribbon Wire 
+<a href="http://vape-atomizer-mesh.com/products/Kanthal-A1-Ribbon-Heat-Resistance-Wire---1mm-x-0.1mm--0.039--x-0.004-----10-Metre-Spool-2551.html" target="_blank">10 meters</a> / 
+<a href="http://vape-atomizer-mesh.com/products/Kanthal-A1-Ribbon-Heat-Resistance-Wire---1mm-x-0.1mm--0.039--x-0.004-----20-Metre-Spool-2552.html" target="_blank">20 meters</a> / 
+<a href="http://vape-atomizer-mesh.com/products/Kanthal-A1-Ribbon-Heat-Resistance-Wire---1mm-x-0.1mm--0.039--x-0.004-----50-Metre-Spool-2553.html" target="_blank">50 meters</a><br> 
+<br>
+The pyrex RSST is a cheap and easy genesis style atomizer (and partial clone of the <a href="http://www.shop.vapinaze.com/" target="_blank">AC9 by Vapinaze</a>). 
+The only issues I have with it are the centre insulators melting (whilst pulsing) but it's a very easy and cheap atomizer to build. I usually recommend <a href="http://www.vapegear.co.uk/Smok_Pyrex_RSST/p1826719_11987849.aspx" target="_blank">Vape 
+Gear</a> if you want to pick one up. This guide should work with most genesis atomizers and I've used a similar method to coil over 20 different styles. 
+The usual issue that can occur is wrapping around the centre pin. <br><br>
+
+<b>If you are planning on running less than a 1ohm coil, or are rebuilding atomizers in general then you need to make sure your device and cells are capable of performing to that level. If you don't understand or care about Ohm's Law, then this tutorial isn't for you.</b>
+
+<br><br><b>
+-Barf/8bv
